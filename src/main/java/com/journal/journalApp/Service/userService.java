878b617a142d305631bc95b1cd49a8a31749d4ba
Sattwik13 <sattwikmanna6359@gmail.com>
2024-09-24 13:00:23 +0530

    Logging Level(Categorized log statements  based)

--- src/main/java/com/journal/journalApp/Service/userService.java
+++ src/main/java/com/journal/journalApp/Service/userService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,20 +18,36 @@ import com.journal.journalApp.entity.userEntry;
 // import com.journal.journalApp.repository.journalEntryRepository;
 import com.journal.journalApp.repository.userRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 @Controller
+@Slf4j
 public class userService {
 
     @Autowired
     private userRepository UserRepository;
 
+    // @Autowired
+    // private static final Logger logger = LoggerFactory.getLogger(userService.class); // @slf4j annotation use substitute of this line
+
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public void saveNewUser(userEntry user) {
         // journalEntry.setData(LocalDateTime.now());
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        UserRepository.save(user);
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            UserRepository.save(user);
+        } catch (Exception e) {
+            // logger.info("hahahahaha");
+            // logger.error("Error occured", e);
+            // logger.error("Error occured {}",user.getUserName(), e);
+            log.error("Error occured {}",user.getUserName(), e); // --> when slf4j annotaion use then log instance create, that's reason use log
+            log.debug("hahahahaha");
+
+
+        }
     }
 
     public void saveAdmin(userEntry user) {
